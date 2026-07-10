b8caf778be1c70e7c110d5d5b878a298d1d68f13
attaganteng012-blip <attaganteng012@gmail.com>
2026-07-10 11:02:14 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var angka = [1,2,3,9,5,1,324,215,575,123];
+
+for (var i = 0; i < angka.length; i++) {
+    if (angka[i] % 2 === 0) {
+        console.log(angka[i] + " Adalah Genap");
+    } else {
+        console.log(angka[i] + " Adalah Ganjil");
+    }
+}
+
